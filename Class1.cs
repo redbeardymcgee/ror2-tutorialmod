97fa00e191ff2e8715f5daa4fa9fcc319e28decf
josh mcgee <josh.s.mcgee@gmail.com>
2021-02-11 18:53:46 -0600

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,16 @@
+﻿using BepInEx;
+using RoR2;
+
+namespace MyUserName
+{
+    [BepInDependency("com.bepis.r2api")]
+    //Change these
+    [BepInPlugin("com.MyCompanyName.MyModName", "My Mod's Title and if we see this exact name on Thunderstore we will deprecate your mod", "1.0.0")]
+    public class MyModName : BaseUnityPlugin
+    {
+        public void Awake()
+        {
+            Logger.LogMessage("Loaded MyModName!");
+        }
+    }
+}
